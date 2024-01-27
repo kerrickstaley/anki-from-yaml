310d143ec4feb5e5b2d48f9218dce546d547c646
Kerrick Staley <k@kerrickstaley.com>
2024-01-26 23:21:35 -0500

    Initial working version

---
+++ main.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import genanki
+import yaml
+import argparse
+import random
+
+parser = argparse.ArgumentParser('Generate an Anki deck from a YAML file')
+parser.add_argument('in_file', help='Input YAML file')
+
+def main(args):
+    with open(args.in_file) as f:
+        data = yaml.safe_load(f)
+
+    deck = genanki.Deck(random.randrange(1 << 31, 1 << 32), data['name'])
+
+    for note_data in data['notes']:
+        note = genanki.Note(
+            model=genanki.builtin_models.CLOZE_MODEL,
+            fields=[note_data['cloze'], ''])
+
+        deck.add_note(note)
+
+    deck.write_to_file(args.in_file.removesuffix('.yaml') + '.apkg')
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    main(args)
